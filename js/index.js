fddf91b78cab2868aaf57959e065a84eceea0f3c
bilzipap <alarconbenjamin37@gmail.com>
2025-08-12 19:19:13 -0400

    alerta like

--- js/index.js
+++ js/index.js
@@ -12,4 +12,13 @@ let botondef = document.querySelector("#updef");
 
 botondef.addEventListener('click', function(){
     this.remove()
+});
+
+let megusta = document.querySelector(".Likecount")
+let namedef = document.querySelector(".Nombre_gato")
+let btnmegusta = document.querySelector(".botonmegusta")
+
+
+btnmegusta.addEventListener('click', function(){
+    alert(namedef.textContent +' '+ "le diste like!!")
 })
