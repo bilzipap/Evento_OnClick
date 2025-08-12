704582f4197c70998d2f03ed1669d74e5cdb1549
bilzipap <alarconbenjamin37@gmail.com>
2025-08-12 18:47:19 -0400

    arreglo funcion cerrarsesion

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ let cerrarsesion = document.querySelector("#iniciarsesion");
 cerrarsesion.addEventListener('click', function(){
     if(cerrarsesion.textContent === "Iniciar Sesión"){
         cerrarsesion.textContent = "Cerrar Sesión"
+    }else if(cerrarsesion.textContent === "Cerrar Sesión"){
+        cerrarsesion.textContent = "Iniciar Sesión"
     }else{
-        cerrarsesion.textContent = "Iniciar Sesion"
+        cerrarsesion.textContent = "Cerrar Sesión"
     }
 });
+
