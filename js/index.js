5d42c2b718c36c1c50f7ec74e537645b246a5f40
bilzipap <alarconbenjamin37@gmail.com>
2025-08-12 19:02:58 -0400

    remover def

--- js/index.js
+++ js/index.js
@@ -3,10 +3,13 @@ let cerrarsesion = document.querySelector("#iniciarsesion");
 cerrarsesion.addEventListener('click', function(){
     if(cerrarsesion.textContent === "Iniciar Sesión"){
         cerrarsesion.textContent = "Cerrar Sesión"
-    }else if(cerrarsesion.textContent === "Cerrar Sesión"){
-        cerrarsesion.textContent = "Iniciar Sesión"
     }else{
-        cerrarsesion.textContent = "Cerrar Sesión"
+        cerrarsesion.textContent = "Iniciar Sesión"
     }
 });
 
+let botondef = document.querySelector("#updef");
+
+botondef.addEventListener('click', function(){
+    this.remove()
+})
